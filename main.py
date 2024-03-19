2c97c603a42ace79b12afb99aea5ac11761bde2a
prathapsona <prathapkaasi@gmail.com>
2024-03-19 11:07:23 +0530

    streamlit df added

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import streamlit as st
-
+import requests
+import pandas as pd
+st.set_page_config(page_title="cheran_app",layout="wide", page_icon="🅿️" )
 st.header("Dashboard")
 st.write("this is some text")
 st.write("this is automation mail feature available")
+
+res = requests.get("https://fakestoreapi.com/products")
+data = res.json()
+df = pd.DataFrame(data)
+st.dataframe(df)
